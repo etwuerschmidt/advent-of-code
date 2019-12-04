9ef98a1bb2c95d8d259ff706f4e2c135f6821bf0
Eric Wuerschmidt <ericwuerschmidt@gmail.com>
2019-12-03 22:13:30 -0500

    Day 1 solution

---
+++ 2019/Day_1/Puzzle_1/solution.py
@@ -0,0 +1,18 @@
+import math
+import pytest
+
+def compute_fuel(filename):
+    modules = []
+    with open(filename, 'r') as f:
+        modules = [int(line.strip()) for line in f]
+
+    total = 0
+    for module in modules:
+        total += math.floor(module / 3) - 2
+    return total
+
+def test_compute_fuel():
+    assert compute_fuel('test_input.txt') == 2 + 2 + 654 + 33583
+
+if __name__ == "__main__":
+    print(compute_fuel('puzzle_input.txt'))
---
+++ 2019/Day_1/Puzzle_2/solution.py
@@ -0,0 +1,23 @@
+import math
+import pytest
+
+def compute_fuel(filename):
+    modules = []
+    with open(filename, 'r') as f:
+        modules = [int(line.strip()) for line in f]
+
+    total = 0
+    for module in modules:
+        module_total = 0
+        while module > 0:
+            module = math.floor(module / 3) - 2
+            if module > 0:
+                module_total += module
+        total += module_total
+    return total
+
+def test_compute_fuel():
+    assert compute_fuel('test_input.txt') == 2 + 2 + 966 + 50346
+
+if __name__ == "__main__":
+    print(compute_fuel('puzzle_input.txt'))
